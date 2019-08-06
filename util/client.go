8bbbf6aaac32fc96e333694223482ac5726b0b77
Everton Macedo <evertonmc@outlook.com>
2019-08-06 18:25:17 +0200

    autorest

--- util/client.go
+++ util/client.go
@@ -1,15 +1,17 @@
 package util
 
 import (
-	kvauth "github.com/Azure/azure-sdk-for-go/services/keyvault/auth"
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/adal"
+	"github.com/Azure/go-autorest/autorest/azure/auth"
 	"github.com/Azure/azure-sdk-for-go/services/keyvault/v7.0/keyvault"
 	"os"
 )
 
 func GetBasicClient() (keyvault.BaseClient, error) {
-	authorizer, err := kvauth.NewAuthorizerFromEnvironment()
+	authorizer, err := autorest.NewAuthorizerFromEnvironment()
 	if err != nil {
-		deviceConfig := kvauth.NewDeviceFlowConfig(os.Getenv("AZURE_CLIENT_ID"), os.Getenv("AZURE_TENANT_ID"))
+		deviceConfig := autorest.NewDeviceFlowConfig(os.Getenv("AZURE_CLIENT_ID"), os.Getenv("AZURE_TENANT_ID"))
 		authorizer, derr := deviceConfig.Authorizer()
 		if derr != nil {
 			return keyvault.New(), derr
