1011cd9bebbf8396b61d703ced6ea8fddd0a0f19
Everton Macedo <evertonmc@outlook.com>
2019-08-06 18:16:31 +0200

    add device app id

--- util/client.go
+++ util/client.go
@@ -3,12 +3,13 @@ package util
 import (
 	kvauth "github.com/Azure/azure-sdk-for-go/services/keyvault/auth"
 	"github.com/Azure/azure-sdk-for-go/services/keyvault/v7.0/keyvault"
+	"os"
 )
 
 func GetBasicClient() (keyvault.BaseClient, error) {
 	authorizer, err := kvauth.NewAuthorizerFromEnvironment()
 	if err != nil {
-		deviceConfig := kvauth.NewDeviceFlowConfig(applicationID, tenantID)
+		deviceConfig := kvauth.NewDeviceFlowConfig(os.Getenv("AZURE_CLIENT_ID"), os.Getenv("AZURE_TENANT_ID"))
 		authorizer, derr := deviceConfig.Authorizer()
 		if derr != nil {
 			return keyvault.New(), derr
